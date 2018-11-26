551941a7e072403bc07e985098f496a989b2d90b
Ahmed Esam <a.esam@greentech.sa>
2018-11-26 12:29:35 +0200

    uploading initial project

---
+++ openWeatherMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  openWeatherMap
+//
+//  Created by GreenTech on 11/26/18.
+//  Copyright © 2018 AhmedKhidr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
